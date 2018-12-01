03b5efcfc8dcfcf36267d8af8b4a25e5962c36bf
Cameron Beeler <cameron.beeler@gmail.com>
2018-11-30 22:23:52 -0800

    testing out comparable as lambda

--- cambeeler.controller/src/cambeeler/controller/Controller.java
+++ cambeeler.controller/src/cambeeler/controller/Controller.java
@@ -2,7 +2,11 @@ package cambeeler.controller;
 
 import javafx.fxml.FXML;
 
+import java.util.*;
+
 public class Controller {
+
+    List<Employee> employee = new ArrayList<>();
 
     @FXML public void iGotPushed()
     {
@@ -22,5 +26,66 @@ public class Controller {
 //                System.out.println("Printing from Runnable thread - iGotPushed");
 //            }
 //        }).start();
+        employee.add(new Employee("Cameron", 50));
+        employee.add(new Employee("Kim", 48));
+        employee.add(new Employee("Shiloh", 18));
+        employee.add(new Employee("Wyatt", 14));
+        employee.add(new Employee("Ani", 16));
+
+//        Collections.sort(employee, new Comparator<Employee>()
+//        {
+//            @Override
+//            public
+//            int compare(Employee first, Employee second)
+//            {
+//                return first.getName().compareToIgnoreCase(second.getName());
+//            }
+//        });
+
+        Collections.sort(employee, (employee1, employee2)->
+        {return employee1.getName().compareToIgnoreCase(employee2.getName());});
+
+
+        for(Employee emp:employee)
+        {
+            System.out.println(emp.getName());
+        }
+    }
+}
+
+class Employee
+{
+    private String name;
+    private int age;
+
+    public
+    Employee(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public
+    String getName()
+    {
+        return name;
+    }
+
+    public
+    void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public
+    int getAge()
+    {
+        return age;
+    }
+
+    public
+    void setAge(int age)
+    {
+        this.age = age;
     }
 }
