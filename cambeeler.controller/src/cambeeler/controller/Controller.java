25d690e77f9aca6ca8917aa63794157d882041ea
Cameron Beeler <cameron.beeler@gmail.com>
2018-11-30 21:49:16 -0800

    inner anonymous class in thread replaced as a lambda...

--- cambeeler.controller/src/cambeeler/controller/Controller.java
+++ cambeeler.controller/src/cambeeler/controller/Controller.java
@@ -7,5 +7,20 @@ public class Controller {
     @FXML public void iGotPushed()
     {
         System.out.println("You pushed the Button!");
+        new Thread(()->
+                   {
+                       System.out.println("This is the lambda execute of the thread - iGotPushed");
+                       System.out.println("This is line 2 of the inner lambda method");
+                   }
+                   ).start();
+//        new Thread(new Runnable(){
+//
+//            @Override
+//            public
+//            void run()
+//            {
+//                System.out.println("Printing from Runnable thread - iGotPushed");
+//            }
+//        }).start();
     }
 }
--- cambeeler.model/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module cambeeler.model {
-//    requires javafx.graphics;
-//    requires javafx.fxml;
-//    exports cambeeler.model to javafx.graphics;
-//    opens cambeeler.model to javafx.fxml;
-}
